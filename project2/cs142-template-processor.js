3da7adb6a21f985158759f7ad71cb088b17cfcac
baigalmaaina <bgl0340@yahoo.com>
2020-10-05 14:10:47 +0800

    id

--- project2/cs142-template-processor.js
+++ project2/cs142-template-processor.js
@@ -4,7 +4,18 @@ function Cs142TemplateProccessor(template)
 }
 Cs142TemplateProccessor.prototype.fillIn=(dictionary);
 {
-    
+    var res=this.template;
+    var re=/{{[^{]*}}/g;
+    var match =this.template.match(re);
+    var pre, key, after;
+    for(var i=0; i<match.length; i++){
+        pre=match[i];
+        key=pre.replace("{{", "");
+        key=key.replace("}}", "");
+        after=dictionary[key] || '';
+        res=res.replace(pre, after);
+    }
+    return res;
 
    
 
